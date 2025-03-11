5e876a707830818222b5309f8a19b099287d57a4
Umarsatti1 <149182950+Umarsatti1@users.noreply.github.com>
2025-03-12 02:18:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def hello_world():
             }
             .container {
                 background: white;
-                padding: 20px;
+                padding: 10px;
                 border-radius: 10px;
                 box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
                 display: inline-block;
