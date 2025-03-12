4b2079488e4376e556bf9982bbb371d7fcb5b9bb
Umarsatti1 <149182950+Umarsatti1@users.noreply.github.com>
2025-03-12 05:01:57 +0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
-#Pull the docker image
+# Pull the latest Docker image
 docker pull umarsatti1/simple-flask-app
 
-#Run the docker image
-docker run -d -p 5000:5000 umarsatti1/simple-flask-app
+# Stop and remove any existing container running on port 5000
+EXISTING_CONTAINER=$(docker ps -q --filter "ancestor=umarsatti1/simple-flask-app")
+if [ -n "$EXISTING_CONTAINER" ]; then
+    echo "Stopping existing container..."
+    docker stop "$EXISTING_CONTAINER"
+    docker rm "$EXISTING_CONTAINER"
+fi
+
+# Run the new container
+docker run -d -p 5000:5000 --name simple-flask-app umarsatti1/simple-flask-app
